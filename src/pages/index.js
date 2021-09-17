7ab5f913c950e1f0348b8fa754f3fc8cf87bcc00
simonsankar <sjsankar10@gmail.com>
2021-09-17 17:41:52 -0400

    Removing block padding on 1st column of home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const IndexPage = () => (
       justifyContent="space-around"
     >
       <Flex direction="column">
-        <Box padding={10}>
+        <Box paddingInline={10}>
           <Text fontFamily="Faustina" fontSize="5xl">
             Hi There,
           </Text>
