2d33d160828a76470c5eff037d828d6b5b5f8e94
simonsankar <sjsankar10@gmail.com>
2021-09-20 13:28:26 -0400

    Adding socials to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,21 @@
 import * as React from "react"
 
-import { Box, Flex, useColorModeValue, Heading, Text } from "@chakra-ui/react"
+import {
+  Box,
+  Flex,
+  useColorModeValue,
+  Text,
+  Stack,
+  IconButton,
+} from "@chakra-ui/react"
 import { Link } from "@chakra-ui/react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { StaticImage } from "gatsby-plugin-image"
+import { EmailIcon } from "@chakra-ui/icons"
 
 const IndexPage = () => (
   <Layout>
@@ -23,40 +33,80 @@ const IndexPage = () => (
       align="center"
       justifyContent="space-around"
     >
-      <Flex direction="column">
-        <Box paddingInline={10} borderLeft="5px solid">
-          <Text fontFamily="Faustina" fontSize="5xl">
-            Hi There,
-          </Text>
-          <span>
-            <Text fontSize="7xl" display="inline" marginRight={2}>
-              I am
-            </Text>
-            <Text
-              fontSize="7xl"
-              fontWeight="bold"
-              color={useColorModeValue("brand.400", "brand.300")}
-              display="inline-block"
-              position="relative"
-            >
-              Simon Sankar
-            </Text>
-          </span>
-          <Text fontFamily="Faustina" fontSize="3xl">
-            Apart from being a{" "}
-            <Link
-              href="https://en.wikipedia.org/wiki/Lionel_Messi"
-              alt="Lionel Messi"
+      <Flex direction="column" flex={3}>
+        <Flex
+          flexDirection={["column-reverse", "row"]}
+          alignItems="center"
+          justify="space-between"
+        >
+          <Stack direction={["row", "column"]} textAlign="center" spacing="4">
+            <IconButton
+              as={"a"}
+              aria-label="linkedin"
+              href="https://www.linkedin.com/in/simon-sankar-5a82a2215/"
               target="_blank"
-              color={useColorModeValue("gray.600", "gray.100")}
+              icon={<FontAwesomeIcon size="2x" icon={faLinkedin} />}
+            />
+            <IconButton
+              as={"a"}
+              aria-label="github"
+              href="https://github.com/simonsankar"
+              target="_blank"
+              icon={<FontAwesomeIcon size="2x" icon={faGithub} />}
+            />
+
+            <IconButton
+              as={"a"}
+              aria-label="email"
+              icon={<EmailIcon boxSize={7} />}
+              href="mailto:sjsankar10@gmail.com"
+              target="_blank"
+            />
+          </Stack>
+          <Box paddingInline={[8, 10, 20, 40]}>
+            <Text
+              fontFamily="Faustina"
+              fontSize={{ base: "2xl", md: "3xl", lg: "5xl", xl: "5xl" }}
             >
-              Lionel Messi
-            </Link>{" "}
-            acoloyte, I build things for the web!
-          </Text>
-        </Box>
+              Hi There,
+            </Text>
+            <span>
+              <Text
+                fontSize={{ base: "4xl", md: "6xl", lg: "7xl", xl: "7xl" }}
+                display="inline"
+                marginRight={2}
+              >
+                I am
+              </Text>
+              <Text
+                fontSize={{ base: "4xl", md: "6xl", lg: "7xl", xl: "7xl" }}
+                fontWeight="bold"
+                color={useColorModeValue("brand.400", "brand.300")}
+                display="inline-block"
+                position="relative"
+              >
+                Simon Sankar
+              </Text>
+            </span>
+            <Text
+              fontFamily="Faustina"
+              fontSize={{ base: "1xl", md: "2xl", lg: "3xl", xl: "5xl" }}
+            >
+              Apart from being a{" "}
+              <Link
+                href="https://en.wikipedia.org/wiki/Lionel_Messi"
+                alt="Lionel Messi"
+                target="_blank"
+                color={useColorModeValue("gray.600", "gray.100")}
+              >
+                Lionel Messi
+              </Link>{" "}
+              acoloyte, I build things for the web!
+            </Text>
+          </Box>
+        </Flex>
       </Flex>
-      <Flex align="center" justify="center">
+      <Flex align="center" justify="center" flex={1}>
         <Box border="5px solid" borderBottomWidth="40px" padding={4}>
           <p>Now go build something great.</p>
           <StaticImage
