a183cee94a5abf73c98b20ffe2fe50e1957e77a5
simonsankar <sjsankar10@gmail.com>
2021-11-01 13:07:49 -0400

    Updates

---
+++ admin/cms.5fdbe2159b6b4bb391d9.hot-update.js
@@ -0,0 +1,33 @@
+"use strict";
+self["webpackHotUpdategatsby_starter_default"]("cms",{
+
+/***/ "./node_modules/ansi-regex/index.js":
+/*!******************************************!*\
+  !*** ./node_modules/ansi-regex/index.js ***!
+  \******************************************/
+/***/ (function(module) {
+
+
+
+module.exports = ({onlyFirst = false} = {}) => {
+	const pattern = [
+		'[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
+		'(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))'
+	].join('|');
+
+	return new RegExp(pattern, onlyFirst ? undefined : 'g');
+};
+
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "9d49220a265aa99b8830"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=cms.5fdbe2159b6b4bb391d9.hot-update.js.map
